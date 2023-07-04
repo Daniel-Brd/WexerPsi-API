0254d5682b54bdd704421a4e53578945fac00716
Daniel-Brd <dev.danielbrandao@gmail.com>
2023-07-04 19:48:48 -0300

    feat: Create findByEmail schema and fix uncknown keys validation

--- src/app/user/schemas/userSchema.ts
+++ src/app/user/schemas/userSchema.ts
@@ -1,9 +1,23 @@
 import * as yup from "yup";
 
 export const makeCreateUserSchema = () => {
-  return yup.object().shape({
-    name: yup.string().required(),
-    email: yup.string().email().required(),
-    password: yup.string().required(),
-  });
+  return yup
+    .object()
+    .shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+      password: yup.string().required(),
+    })
+    .noUnknown()
+    .strict();
+};
+
+export const makeFindByEmailUserSchema = () => {
+  return yup
+    .object()
+    .shape({
+      email: yup.string().email().required(),
+    })
+    .noUnknown()
+    .strict();
 };
