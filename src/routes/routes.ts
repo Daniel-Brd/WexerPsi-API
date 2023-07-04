4ea02b97a1c5abb77f0719de64c162c02b89ad58
Daniel-Brd <dev.danielbrandao@gmail.com>
2023-07-04 20:43:28 -0300

    feat: Create deleteUserById route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -6,3 +6,4 @@ export const router = Router();
 const userController = userModule.build().controller;
 
 router.post("/user", userController.create.bind(userController));
+router.delete("/user/:id", userController.deleteById.bind(userController));
