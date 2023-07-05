9ea961aa8aa9d74b559070d2237eb44e65cfc93d
Daniel-Brd <dev.danielbrandao@gmail.com>
2023-07-05 20:43:42 -0300

    feat: Create authModule

---
+++ src/app/auth/authModule.ts
@@ -0,0 +1,13 @@
+import { UserModule } from "../user/UserModule";
+import { AuthController } from "./controllers/authController";
+import { AuthService } from "./services/authService";
+
+export class AuthModule {
+  static build() {
+    const repository = UserModule.build().repository;
+    const service = new AuthService(repository);
+    const controller = new AuthController(service);
+
+    return { repository, service, controller };
+  }
+}
