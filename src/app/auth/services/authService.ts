01469c41fe7602202460bf5435a4d88de945310b
Daniel-Brd <dev.danielbrandao@gmail.com>
2023-07-05 20:43:37 -0300

    feat: Create authService

---
+++ src/app/auth/services/authService.ts
@@ -0,0 +1,37 @@
+import { compareSync } from "bcrypt";
+import * as JWT from "jsonwebtoken";
+
+import { UserRepository } from "../../user/repositories/userRepository";
+import { LoginDto } from "../dtos/loginDto";
+
+export class AuthService {
+  constructor(private repository: UserRepository) {}
+
+  async login(body: LoginDto) {
+    const user = await this.repository.findByEmail(body.email);
+
+    if (!user) {
+      return { error: true, message: "Invalid e-mail or password", status: 400 };
+    }
+
+    console.log(user.password);
+
+    const isPasswordValid = compareSync(body.password, user.password);
+
+    if (!isPasswordValid) {
+      return { error: true, message: "Invalid e-mail or password", status: 400 };
+    }
+
+    const payload = { id: user.id };
+
+    const secretKey = process.env.JWT_SECRET_KEY as string;
+
+    const options = { expiresIn: "60min" };
+
+    const token = JWT.sign(payload, secretKey, options);
+
+    const result = { token, user };
+
+    return result;
+  }
+}
