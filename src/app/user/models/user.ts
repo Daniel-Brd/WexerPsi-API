3007b62a28783ba83e44f3955266a2abf575322a
Daniel-Brd <dev.danielbrandao@gmail.com>
2023-07-05 20:48:52 -0300

    refactor: Add select: false to password in UserSchema

--- src/app/user/models/user.ts
+++ src/app/user/models/user.ts
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true, select: false },
     patients: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Patient" }],
     file: { type: mongoose.SchemaTypes.ObjectId, ref: "File" },
   },
