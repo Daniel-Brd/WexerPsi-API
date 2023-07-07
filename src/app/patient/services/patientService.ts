5909974cdb0e917214bdaf6539fed6970eb490b6
Daniel Brandão <dev.danielbrandao@gmail.com>
2023-07-07 16:06:39 -0300

    feat: Create update patient service

--- src/app/patient/services/patientService.ts
+++ src/app/patient/services/patientService.ts
@@ -1,5 +1,6 @@
 import { UserRepository } from "../../user/repositories/userRepository";
 import { CreatePatientServiceDTO } from "../dtos/createPatientServiceDto";
+import { UpdatePatientDTO } from "../dtos/updatePatientDto";
 import { PatientRepository } from "../repositories/patientRepository";
 
 export class PatientService {
@@ -46,6 +47,24 @@ export class PatientService {
       return this.repository.getPatientById(id);
     } catch (err) {
       return { error: true, message: "Patient not found", status: 404 };
+    }
+  }
+
+  async updatePatient(id: string, body: UpdatePatientDTO) {
+    const patient = await this.repository.getPatientById(id);
+
+    if (!Boolean(Object.keys(body).length)) {
+      return { error: true, message: "Empty body", status: 400 };
+    }
+
+    if (!patient) {
+      return { error: true, message: "Patient not found", status: 404 };
+    }
+
+    try {
+      return this.repository.updatePatient(id, body);
+    } catch (err) {
+      return { error: true, message: "Internal server error", status: 500 };
     }
   }
 }
