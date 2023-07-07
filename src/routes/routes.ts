af541711c4d973f06736b2cfdf549b5701cc51eb
Daniel-Brd <dev.danielbrandao@gmail.com>
2023-07-06 21:41:32 -0300

    feat: Add create patient route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { UserModule } from "../app/user/userModule";
 import { AuthModule } from "../app/auth/authModule";
 import { EnsureAuth } from "../common/middleares/ensureAuth";
+import { PatientModule } from "../app/patient/patientModule";
 
 export const router = Router();
 
 const userController = UserModule.build().controller;
 const authController = AuthModule.build().controller;
+const patientController = PatientModule.build().controller;
 
 router.post("/user", userController.create.bind(userController));
 router.post("/login", authController.login.bind(authController));
@@ -15,3 +17,4 @@ router.use(EnsureAuth.ensureAuth);
 
 router.patch("/user/:id", userController.updateById.bind(userController));
 router.delete("/user/:id", userController.deleteById.bind(userController));
+router.post("/user/:id/patient", patientController.create.bind(patientController));
