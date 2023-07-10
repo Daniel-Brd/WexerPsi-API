7a8c846075061b61408491178cd07759e691e0d7
Daniel-Brd <dev.danielbrandao@gmail.com>
2023-07-10 18:03:46 -0300

    feat: Create associate a timeline repository

--- src/app/patient/repositories/patientRepository.ts
+++ src/app/patient/repositories/patientRepository.ts
@@ -4,17 +4,11 @@ import { UpdatePatientDTO } from "../dtos/updatePatientDto";
 import { Patient } from "../models/patient";
 
 export class PatientRepository {
-  constructor(
-    private model: typeof Patient,
-    private userRepository: UserRepository
-  ) {}
+  constructor(private model: typeof Patient, private userRepository: UserRepository) {}
 
   async create(patient: CreatePatientDTO) {
     const result = await this.model.create(patient);
-    this.userRepository.associatePatient(
-      result.user.toString(),
-      result._id.toString()
-    );
+    this.userRepository.associatePatient(result.user.toString(), result._id.toString());
     return result;
   }
 
@@ -24,10 +18,16 @@ export class PatientRepository {
   }
 
   async getPatientById(id: string) {
-    return this.model.findById(id);
+    return this.model.findById(id).populate("timelines");
   }
 
   async updatePatient(id: string, payload: UpdatePatientDTO) {
     return this.model.findByIdAndUpdate(id, payload, { new: true });
+  }
+
+  async associateTimeline(id: string, timelineId: string) {
+    return this.model.findByIdAndUpdate(id, {
+      $push: { timelines: [timelineId] },
+    });
   }
 }
