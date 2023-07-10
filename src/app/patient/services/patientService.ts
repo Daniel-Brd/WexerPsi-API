1a8e3477e53797b4e0a274ab070ec30b10d86c99
Daniel-Brd <dev.danielbrandao@gmail.com>
2023-07-10 19:29:53 -0300

    feat: Create get timelines by patient service

--- src/app/patient/services/patientService.ts
+++ src/app/patient/services/patientService.ts
@@ -4,10 +4,7 @@ import { UpdatePatientDTO } from "../dtos/updatePatientDto";
 import { PatientRepository } from "../repositories/patientRepository";
 
 export class PatientService {
-  constructor(
-    private repository: PatientRepository,
-    private userRepositpry: UserRepository
-  ) {}
+  constructor(private repository: PatientRepository, private userRepositpry: UserRepository) {}
 
   async create({ body, userId }: CreatePatientServiceDTO) {
     const payload = {
@@ -51,7 +48,7 @@ export class PatientService {
   }
 
   async updatePatient(id: string, body: UpdatePatientDTO) {
-    const patient = await this.repository.getPatientById(id);
+    const patient = await this.getPatientById(id);
 
     if (!Boolean(Object.keys(body).length)) {
       return { error: true, message: "Empty body", status: 400 };
@@ -63,6 +60,21 @@ export class PatientService {
 
     try {
       return this.repository.updatePatient(id, body);
+    } catch (err) {
+      return { error: true, message: "Internal server error", status: 500 };
+    }
+  }
+
+  async getAllPatientTimelines(id: string) {
+    const isPatientValid = await this.getPatientById(id);
+
+    if (!isPatientValid) {
+      return { error: true, message: "Patient not found", status: 404 };
+    }
+
+    try {
+      const result = await this.repository.getAllPatientTimelines(id);
+      return result?.timelines;
     } catch (err) {
       return { error: true, message: "Internal server error", status: 500 };
     }
