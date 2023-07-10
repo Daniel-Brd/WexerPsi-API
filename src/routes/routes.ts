9e3d6a84026b220f4c79e11285a373e0658982d2
Daniel-Brd <dev.danielbrandao@gmail.com>
2023-07-10 18:08:40 -0300

    feat: Add create timeline route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,33 +3,24 @@ import { UserModule } from "../app/user/userModule";
 import { AuthModule } from "../app/auth/authModule";
 import { EnsureAuth } from "../common/middleares/ensureAuth";
 import { PatientModule } from "../app/patient/patientModule";
+import { TimelineModule } from "../app/timeline/timelineModule";
 
 export const router = Router();
 
 const userController = UserModule.build().controller;
 const authController = AuthModule.build().controller;
 const patientController = PatientModule.build().controller;
+const timelineController = TimelineModule.build().controller;
 
-router.post("/user", userController.create.bind(userController));
+router.post("/users", userController.create.bind(userController));
 router.post("/login", authController.login.bind(authController));
 
 router.use(EnsureAuth.ensureAuth);
 
-router.patch("/user/:id", userController.updateById.bind(userController));
-router.delete("/user/:id", userController.deleteById.bind(userController));
-router.post(
-  "/user/:id/patient",
-  patientController.create.bind(patientController)
-);
-router.get(
-  "/user/:id/patient",
-  patientController.getPatientByUser.bind(patientController)
-);
-router.get(
-  "/patient/:id",
-  patientController.getPatientById.bind(patientController)
-);
-router.patch(
-  "/patient/:id",
-  patientController.updatePatient.bind(patientController)
-);
+router.patch("/users/:id", userController.updateById.bind(userController));
+router.delete("/users/:id", userController.deleteById.bind(userController));
+router.post("/users/:id/patients", patientController.create.bind(patientController));
+router.get("/users/:id/patients", patientController.getPatientByUser.bind(patientController));
+router.get("/patients/:id", patientController.getPatientById.bind(patientController));
+router.patch("/patients/:id", patientController.updatePatient.bind(patientController));
+router.post("/patients/:id/timelines", timelineController.create.bind(timelineController));
