d41c42692d08bed1de04f1c6963afee2022e5046
Daniel-Brd <dev.danielbrandao@gmail.com>
2023-07-11 19:11:55 -0300

    feat: feat: Create update timeline service

--- src/app/timeline/services/timelineService.ts
+++ src/app/timeline/services/timelineService.ts
@@ -1,5 +1,5 @@
-import { PatientRepository } from "../../patient/repositories/patientRepository";
 import { CreateTimelineServiceDto } from "../dtos/createTimelineServiceDto";
+import { UpdateTimelineDTO } from "../dtos/updateTimelineDto";
 import { TimelineRepository } from "../repositories/timelineRepository";
 
 export class TimelineService {
@@ -26,5 +26,23 @@ export class TimelineService {
     }
 
     return timeline;
+  }
+
+  async updateTimeline(id: string, body: UpdateTimelineDTO) {
+    const isTimelineValid = await this.getTimelineById(id);
+
+    if (!Boolean(Object.keys(body).length)) {
+      return { error: true, message: "Empty body", status: 400 };
+    }
+
+    if (!isTimelineValid) {
+      return { error: true, message: "Timeline not found", status: 404 };
+    }
+
+    try {
+      return this.repository.updateTimeline(id, body);
+    } catch (err) {
+      return { error: true, message: "Internal server error", status: 500 };
+    }
   }
 }
