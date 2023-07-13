c52ba188e13c8b7a5137c9cd6bafc5300ecc6588
Daniel-Brd <dev.danielbrandao@gmail.com>
2023-07-12 22:56:18 -0300

    fix: Allow only specified strings in occurrence kind key

--- src/app/occurrence/schemas/occurrenceSchema.ts
+++ src/app/occurrence/schemas/occurrenceSchema.ts
@@ -6,7 +6,7 @@ export const makeCreateOccurrenceSchema = () => {
     .shape({
       title: yup.string().required(),
       content: yup.string().required(),
-      kind: yup.string().required(),
+      kind: yup.string().oneOf(["Sessão", "Fato Relevante"]).required(),
       files: yup.array().of(yup.string()),
     })
     .noUnknown()
